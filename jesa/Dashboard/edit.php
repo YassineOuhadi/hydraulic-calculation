0651f43a42ec2e8cef85f5ebb7fba21eedc1ddbd
Yassine Ouhadi <109771302+YassineOuhadi@users.noreply.github.com>
2022-11-08 21:39:17 +0100

    Delete jesa directory

--- jesa/Dashboard/edit.php
+++
@@ -1,8 +0,0 @@
-<?php
-include_once '../include/Classes.php';
-$bdd=fct_bdd();
-$ManagerProjet = new ManagerProjet($bdd);
-$idProjet=$_POST['idProjet'];
-$titre=$_POST['titre'];
-$ManagerProjet->edit_projet($idProjet,$titre);   
-?>
--- jesa/Dashboard2/delete.php
+++
@@ -1,7 +0,0 @@
-<?php
-include_once '../include/Classes.php';
-$bdd=fct_bdd();
-$ManagerProjet = new ManagerProjet($bdd);
-$id=$_POST['id'];
-$ManagerProjet->supprimer_piece($id);
-?>
