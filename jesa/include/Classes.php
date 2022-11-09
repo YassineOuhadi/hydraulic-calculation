88b87107dd17326b737de233db65a0a347698057
Yassine Ouhadi <109771302+YassineOuhadi@users.noreply.github.com>
2022-11-09 01:21:12 +0100

    Delete jesa directory

--- jesa/include/Classes.php
+++
@@ -1,8 +0,0 @@
-<?php 
-include_once '../connectiondb.php';
-include_once '../Classes/ClasseCompte.php';
-include_once '../Classes/ClasseProjet.php';
-include_once '../Classes/ClasseTrajet.php';
-include_once '../Classes/ManagerProjet.php';
-include_once '../Classes/ClassePiece.php';
-?>
--- jesa/lignes/save.php
+++
@@ -1,3 +0,0 @@
-<?php
-header("Location:../projets/index.php");
-?>
